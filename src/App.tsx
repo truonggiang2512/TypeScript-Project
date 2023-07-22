b6b798798f578f5264ff444413dfcb46b0815e49
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2023-07-22 14:34:42 +0700

    install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import MdPhone from "@mui/icons-material/Phone";
 function App() {
-  const [count, setCount] = useState(0);
-  const docute = () => {
-    ewqewq;
-  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Docute</div>
+      <Button variant="contained">Hello World</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+
+      <Chip icon={<MdPhone />} label="Docuteeeé" />
     </>
   );
 }
